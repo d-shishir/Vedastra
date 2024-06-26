eb3db3c7772b02719b2fe48bfece1ab8b2a1f0f7
Shishir Lamichhane <dshishir13@gmail.com>
2024-06-26 18:40:24 +0545

    add user model

---
+++ vedastra-backend/models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  birthdate: { type: Date, required: true },
+  birthtime: { type: String },
+  birthplace: { type: String },
+  profilePicture: { type: String },
+  preferences: {
+    dailyHoroscope: { type: Boolean, default: true },
+    personalizedReadings: { type: Boolean, default: true },
+  },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("User", UserSchema);
