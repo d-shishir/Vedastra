80317f45d15ef976ee4592f6d7bb0004f5a98aee
Shishir Lamichhane <dshishir13@gmail.com>
2024-06-26 18:49:56 +0545

    update app.js

--- vedastra-backend/app.js
+++ vedastra-backend/app.js
@@ -1,5 +1,23 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const errorHandler = require("./middlewares/errorHandler");
+require("dotenv").config();
+
+// Connect to the database
+connectDB();
 
 const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/astrologers", require("./routes/astrologerRoutes"));
+app.use("/api/consultations", require("./routes/consultationRoutes"));
+app.use("/api/dailyHoroscopes", require("./routes/dailyHoroscopeRoutes"));
+
+// Error handling middleware
+app.use(errorHandler);
 
 module.exports = app;
